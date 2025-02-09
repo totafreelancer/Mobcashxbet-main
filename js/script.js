8b3fdba93edf7ccc9f8a9452ea1a2a08eb8d8a1c
totafreelancer <totamotiur@gmail.com>
2025-02-09 23:33:29 +0600

    hds

--- js/script.js
+++ js/script.js
@@ -100,3 +100,76 @@ $(document).ready(function () {
     checkScreenSize();
   });
 });
+
+
+$(document).ready(function () {
+  var slider = $(".responsives2");
+
+  // Initialize the Slick Slider
+  function initializeSlick() {
+    slider.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  }
+
+
+    // Custom navigation buttons
+    $(".memo-opportunities__list--nav-item.prev").click(function() {
+      slider.slick("slickPrev"); // Move to previous slide
+    });
+  
+    $(".memo-opportunities__list--nav-item.next").click(function() {
+      slider.slick("slickNext"); // Move to next slide
+    });
+
+  // Check for screen size on load and resize
+  function checkScreenSize() {
+    if ($(window).width() > 1024) {
+      if (slider.hasClass("slick-initialized")) {
+        // Uninitialize Slick (remove it completely)
+        slider.slick("unslick");
+        // Add any additional CSS changes here, if needed, for desktop
+        $(".memo-app-slider__inner_list-item").css("display", "block");
+      }
+    } else {
+      if (!slider.hasClass("slick-initialized")) {
+        // Initialize Slick again for mobile/tablet screens
+        initializeSlick();
+      }
+    }
+  }
+
+  // Initialize on page load
+  checkScreenSize();
+
+  // Check on window resize
+  $(window).resize(function () {
+    checkScreenSize();
+  });
+});
